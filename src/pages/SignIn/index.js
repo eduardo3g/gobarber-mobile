6273af55c4fa00d5ad84f7ca21499de00baa972c
= <=>
2019-12-07 17:49:01 -0300

    chore: Connecting SignIn and SignUp pages to Redux, returning JWT and storing auth/profile information on Redux. Also added loading to both pages buttons.

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,9 +1,11 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { Image } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
 
 import logo from '~/assets/logo.png';
 
 import Background from '~/components/Background';
+import { signInRequest } from '~/store/modules/auth/actions';
 
 import {
   Container,
@@ -15,9 +17,17 @@ import {
 } from './styles';
 
 export default function SignIn({ navigation }) {
+  const dispatch = useDispatch();
   const passwordRef = useRef();
 
-  function handleSubmit() {}
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const loading = useSelector(state => state.auth.loading);
+
+  function handleSubmit() {
+    dispatch(signInRequest(email, password));
+  }
 
   return (
     <Background>
@@ -32,6 +42,8 @@ export default function SignIn({ navigation }) {
             placeholder="Digite seu e-mail"
             returnKeyType="next" // Adiciona botão "Next"
             onSubmitEditing={() => passwordRef.current.focus()} // Ao pressionar o botão, aplicar focus no campo de senha
+            value={email}
+            onChangeText={setEmail}
           />
           <FormInput
             icon="lock-outline"
@@ -40,9 +52,13 @@ export default function SignIn({ navigation }) {
             ref={passwordRef}
             returnKeyType="send" // Como é o último input a ser preenchido (senha), adicionar botão de submit no teclado
             onSubmitEditing={handleSubmit} // disparar função de submit do formulário de SignIn
+            value={password}
+            onChangeText={setPassword}
           />
 
-          <SubmitButton onPress={handleSubmit}>Acessar</SubmitButton>
+          <SubmitButton loading={loading} onPress={handleSubmit}>
+            Acessar
+          </SubmitButton>
         </Form>
 
         <SignLink onPress={() => navigation.navigate('SignUp')}>
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,9 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { Image } from 'react-native';
+import { useDispatch } from 'react-redux';
 
 import logo from '~/assets/logo.png';
+import { signUpRequest, useSelector } from '~/store/modules/auth/actions';
 
 import Background from '~/components/Background';
 
@@ -15,10 +17,20 @@ import {
 } from './styles';
 
 export default function SignUp({ navigation }) {
+  const dispatch = useDispatch();
+
   const emailRef = useRef();
   const passwordRef = useRef();
 
-  function handleSubmit() {}
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const loading = useSelector(state => state.auth.loading);
+
+  function handleSubmit() {
+    dispatch(signUpRequest(name, email, password));
+  }
 
   return (
     <Background>
@@ -33,6 +45,8 @@ export default function SignUp({ navigation }) {
             placeholder="Nome completo"
             returnKeyType="next" // Adiciona botão "Next"
             onSubmitEditing={() => emailRef.current.focus()} // Ao pressionar o botão, aplicar focus no campo de email
+            value={name}
+            onChangeText={setName}
           />
           <FormInput
             icon="mail-outline"
@@ -43,6 +57,8 @@ export default function SignUp({ navigation }) {
             ref={emailRef}
             returnKeyType="next" // Adiciona botão "Next"
             onSubmitEditing={() => passwordRef.current.focus()} // Ao pressionar o botão, aplicar focus no campo de senha
+            value={email}
+            onChangeText={setEmail}
           />
           <FormInput
             icon="lock-outline"
@@ -51,9 +67,13 @@ export default function SignUp({ navigation }) {
             ref={passwordRef}
             returnKeyType="send" // Como é o último input a ser preenchido (senha), adicionar botão de submit no teclado
             onSubmitEditing={handleSubmit} // disparar função de submit do formulário de SignIn
+            value={password}
+            onChangeText={setPassword}
           />
 
-          <SubmitButton onPress={handleSubmit}>Acessar</SubmitButton>
+          <SubmitButton loading={loading} onPress={handleSubmit}>
+            Criar conta
+          </SubmitButton>
         </Form>
 
         <SignLink onPress={() => navigation.navigate('SignIn')}>
