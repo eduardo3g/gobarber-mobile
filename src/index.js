2d87b9d7190f9ff29d582c593bdcab6244157cf2
= <=>
2020-01-31 21:32:55 -0300

    Config: CodePush configuration.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import CodePush from 'react-native-code-push';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { StatusBar } from 'react-native';
@@ -8,13 +9,26 @@ import './config/ReactotronConfig';
 import { store, persistor } from './store';
 import App from './App';
 
-export default function Index() {
-  return (
-    <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-        <App />
-      </PersistGate>
-    </Provider>
-  );
-}
+// export default function Index() {
+//   return (
+//     <Provider store={store}>
+//       <PersistGate persistor={persistor}>
+//         <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+//         <App />
+//       </PersistGate>
+//     </Provider>
+//   );
+// }
+
+const Index = () => (
+  <Provider store={store}>
+    <PersistGate persistor={persistor}>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      <App />
+    </PersistGate>
+  </Provider>
+);
+
+export default CodePush({
+  checkFrequency: CodePush.CheckFrequency.ON_APP_RESUME,
+})(Index);
